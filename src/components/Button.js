5a62597d292fdc5ffd1ec1f2b486da2ec2888429
ReeceLangerock <ReeceLangerock@gmail.com>
2017-08-31 18:30:08 -0500

    add button onClick functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,38 +1,30 @@
-import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as actions from "../actions/actions";
-import "../style/Button.css";
+import React from 'react'
+import { connect } from 'react-redux'
+
+import * as actions from '../actions/actions'
+import '../style/Button.css'
 
 export class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick(event) {
-    console.log(this.props.buttonMethod);
-    this.props[this.props.buttonMethod]();
+  handleClick (event) {
+    console.log(this.props.buttonMethod)
+    this.props[this.props.buttonMethod](this.props.text)
   }
-  render() {
+  render () {
     return (
-      <button className="Button" onClick={this.handleClick}>
+      <button className={`Button Button__${this.props.buttonStyle}`} onClick={this.handleClick}>
         {this.props.buttonName}
       </button>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
   text: state.markdownReducer.text
-});
+})
 
-// const mapDispatchToProps = dispatch =>
-//   bindActionCreators(
-//     {
-//       setMarkdownText
-//     },
-//     dispatch
-//   );
-
-export default connect(mapStateToProps, actions)(Button);
+export default connect(mapStateToProps, actions)(Button)
--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,42 +1,34 @@
-import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as actions from "../actions/actions";
+import React from 'react'
+import { connect } from 'react-redux'
+
+import * as actions from '../actions/actions'
 import Button from './Button'
 import '../style/ButtonContainer.css'
 
 export class ButtonContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleInput = this.handleInput.bind(this);
+  constructor (props) {
+    super(props)
+    this.handleInput = this.handleInput.bind(this)
   }
 
-  handleInput(event) {
-    this.props.setMarkdownText(event.target.value);
+  handleInput (event) {
+    this.props.setMarkdownText(event.target.value)
   }
-  render() {
+  render () {
     return (
-      <div className="ButtonContainer">
-      <div className="ButtonContainer__container">
-        <Button buttonName="Reset" buttonMethod = 'resetMarkdownText'/>
-        <Button buttonName="Save Text" buttonMethod = 'saveText'/>
-        <Button buttonName="Save Markdown"buttonMethod = 'saveMarkdownText' />
+      <div className='ButtonContainer'>
+        <div className='ButtonContainer__container'>
+          <Button buttonName='Reset' buttonMethod='resetMarkdownText' buttonStyle='danger' />
+          <Button buttonName='Save Text' buttonMethod='saveText' buttonStyle='primary' />
+
+        </div>
       </div>
-      </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
   text: state.markdownReducer.text
-});
+})
 
-// const mapDispatchToProps = dispatch =>
-//   bindActionCreators(
-//     {
-//       setMarkdownText
-//     },
-//     dispatch
-//   );
-
-export default connect(mapStateToProps, actions)(ButtonContainer);
+export default connect(mapStateToProps, actions)(ButtonContainer)
