bf2adcbb445455d8cbf5a226b4fd13dfa7acb3d7
ReeceLangerock <ReeceLangerock@gmail.com>
2017-08-31 18:32:30 -0500

    add styling to app for sticky footer, add background image and custom font

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import store from "./store/store";
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
-import Footer from "./components/Footer";
-import MarkdownEditor from "./components/MarkdownEditor";
-import MarkdownDisplay from "./components/MarkdownDisplay";
-import ButtonContainer from "./components/ButtonContainer";
-import "./App.css";
+import Footer from './components/Footer'
+import MarkdownEditor from './components/MarkdownEditor'
+import MarkdownDisplay from './components/MarkdownDisplay'
+import ButtonContainer from './components/ButtonContainer'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Provider store={store}>
         <div>
-          <div className="App__container">
-            <h1>Mark My Words...</h1>
-            <div className="App__markdown-container">
+          <div className='App'>
+            <h1 className='title' >Mark My Words...</h1>
+            <div className='App__markdown-container'>
               <MarkdownEditor />
               <MarkdownDisplay />
             </div>
 
             <ButtonContainer />
-            <Footer />
           </div>
+
+          <Footer />
+
         </div>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
